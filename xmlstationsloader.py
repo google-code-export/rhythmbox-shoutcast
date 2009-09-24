56588e8debe176dcb4ea9bbeb6543e8a568af631
Alexey Kuznetsov <ak@axet.ru>
2009-09-24 19:00:11 +0400

    drop local variables

--- xmlstationsloader.py
+++ xmlstationsloader.py
@@ -6,15 +6,6 @@ from xmlloader import XmlLoader
 
 class XmlStationsLoader(XmlLoader):
 
-  db = None
-  shell = None
-  entry_type = None
-  
-  file_local = None
-  file_local_temp = None
-  file_url = None
-  xml_handler = None
-  
   def __init__(self, genre, cache_dir):
     self.file_local = os.path.join(cache_dir, 'stations-%s.xml' % genre)
     self.file_local_temp = os.path.join(self.file_local, '.tmp')
