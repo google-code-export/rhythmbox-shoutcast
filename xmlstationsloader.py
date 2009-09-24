4d22c3bc0bc1145fc75bae22a48c04c014832ca6
Alexey Kuznetsov <ak@axet.ru>
2009-09-24 17:53:13 +0400

    enter at eof

--- xmlstationsloader.py
+++ xmlstationsloader.py
@@ -19,3 +19,4 @@ class XmlStationsLoader(XmlLoader):
     self.file_local = os.path.join(cache_dir, 'stations-%s.xml' % genre)
     self.file_local_temp = os.path.join(self.file_local, '.tmp')
     self.file_url = 'http://yp.shoutcast.com/sbin/newxml.phtml?genre=%s' % genre
+    self.xml_handler = XmlStationsHandler(self.db, self.entry_type)
