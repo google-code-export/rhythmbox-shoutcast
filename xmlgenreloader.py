ae119bf3f3b46e71c096fc115c80cc7b7c687c43
Alexey Kuznetsov <ak@axet.ru>
2009-09-27 18:33:33 +0400

    add cache dir

--- xmlgenreloader.py
+++ xmlgenreloader.py
@@ -1,13 +1,18 @@
 import rhythmdb
 import xml.sax, xml.sax.handler
 import shutil
+import os, os.path
 
 from xmlloader import XmlLoader
+from xmlgenrehandler import XmlGenreHandler
 
 class XmlGenreLoader(XmlLoader):
-  
-  def __init__(self, db):
+
+  def __init__(self, db, cache_dir, entry_type_s):
     self.db = db
+    self.entry_type = entry_type_s
+    
+    print "DIR IS" + cache_dir
     
     self.file_local = os.path.join(cache_dir, 'genres.xml')
     self.file_local_temp = os.path.join(self.file_local, '.tmp')
