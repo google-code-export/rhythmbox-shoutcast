ef597b7e16a1baff26c0cb5c747b3ac7b721b087
Alexey Kuznetsov <ak@axet.ru>
2009-10-25 21:01:21 +0300

    rename playlist to pl

---
+++ totem-pl-parser/totem-pl-parser-module.c
@@ -0,0 +1,23 @@
+#include <pygobject.h>
+
+extern PyMethodDef totem_pl_parser_functions[];
+
+void totem_pl_parser_add_constants(PyObject *module, const gchar *strip_prefix);
+void totem_pl_parser_register_classes(PyObject *d);
+
+DL_EXPORT(void) inittotem_pl_parser(void)
+{
+  PyObject *m, *d;
+
+  init_pygobject();
+
+  m = Py_InitModule("totem_pl_parser", totem_pl_parser_functions);
+  d = PyModule_GetDict(m);
+
+  totem_pl_parser_register_classes(d);
+  if(PyErr_Occurred()){
+    Py_FatalError ("can't initialise module totem_pl_parser");
+  }
+
+  totem_pl_parser_add_constants(m, "");
+}
