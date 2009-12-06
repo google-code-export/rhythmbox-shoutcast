7d3ea86fe288aa5552a261b7ad7b5450a901d0ce
Alexey Kuznetsov <ak@axet.ru>
2009-12-06 10:10:38 +0300

    add new demo

--- totem-pl-parser/example.py
+++ totem-pl-parser/example.py
@@ -1,27 +1,17 @@
 import totem_pl_parser
 
 def pl_entry(parser, uri, htable):
-  title = htable['title'];
-  #genre = htable.lookup (metadata, TOTEM_PL_PARSER_FIELD_GENRE);
-  print uri, htable
-  print htable.__class__()
+  print 'uri: ' + uri
+  print 'title: ' + htable.lookup('title').__str__()
+  print 'genre: ' + htable.lookup('genre').__str__()
 
 def parse_uri(uri):
-  parser = totem_pl_parser.TotemPlParser()
-  print dir(parser)
-  parser.connect('entry-parsed', pl_entry)
+  parser = totem_pl_parser.TotemPyParser()
+  parser.connect('entry-py-parsed', pl_entry)
   #g_object_set (parser, "recurse", FALSE, NULL);
 
   res = parser.parse(uri, False)
+  
+  return res
 
-  if res == totem_pl_parser.TOTEM_PL_PARSER_RESULT_UNHANDLED:
-    return
-  if res == totem_pl_parser.TOTEM_PL_PARSER_RESULT_IGNORED:
-    return
-
-  if res == totem_pl_parser.TOTEM_PL_PARSER_RESULT_SUCCESS:
-    return
-  if res == totem_pl_parser.TOTEM_PL_PARSER_RESULT_ERROR:
-    return
-
-parse_uri('http://yp.shoutcast.com/sbin/tunein-station.pls?id=9492')
+print parse_uri('http://yp.shoutcast.com/sbin/tunein-station.pls?id=9492')
