f6a1667742967dc4987b0dcddee3c1e4ca4d0d89
Alexey Kuznetsov <ak@axet.ru>
2010-03-14 11:03:50 +0300

    more clean debug output

--- plugin/debug/debug.py
+++ plugin/debug/debug.py
@@ -14,11 +14,14 @@ def fe():
   return Exception(result)
 
 def entry_to_string(db, entry):
-  print "id: " + repr(db.entry_get(entry, rhythmdb.PROP_ENTRY_ID))
-  print "title: " + repr(db.entry_get(entry, rhythmdb.PROP_TITLE))
-  print "genre: " + repr(db.entry_get(entry, rhythmdb.PROP_GENRE))
-  print "url: " + repr(db.entry_get(entry, rhythmdb.PROP_LOCATION))
-  print "keyword: " + repr(db.entry_get(entry, rhythmdb.PROP_KEYWORD))
+  out = ''
+  out += "id: " + repr(db.entry_get(entry, rhythmdb.PROP_ENTRY_ID)) + '\n'
+  out += "title: " + repr(db.entry_get(entry, rhythmdb.PROP_TITLE)) + '\n'
+  out += "genre: " + repr(db.entry_get(entry, rhythmdb.PROP_GENRE)) + '\n'
+  out += "url: " + repr(db.entry_get(entry, rhythmdb.PROP_LOCATION)) + '\n'
+  out += "keyword: " + repr(db.entry_get(entry, rhythmdb.PROP_KEYWORD)) + '\n'
+  
+  print out
 
 def log(log):
   print log
