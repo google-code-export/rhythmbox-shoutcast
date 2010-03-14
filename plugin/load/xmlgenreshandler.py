f5231550117f942ce40b0aedc1081e6ab1452f1b
Alexey Kuznetsov <ak@axet.ru>
2010-03-14 16:25:57 +0300

    unquote

--- plugin/load/xmlgenreshandler.py
+++ plugin/load/xmlgenreshandler.py
@@ -18,7 +18,6 @@ class XmlGenresHandler(xml.sax.handler.ContentHandler):
       genre = self.attrs['name']
       
       track_url = 'http://yp.shoutcast.com/sbin/newxml.phtml?genre=%s' % genre
-      track_url = urllib.quote(track_url)
 
       entry = self.db.entry_lookup_by_location(track_url)
       if entry == None:
